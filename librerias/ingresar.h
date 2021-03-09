c86f2183cdf2aa2e915cf1df5d973d3a2cb7fc51
MateoCueva03 <nicolasmcr99@gmail.com>
2021-03-08 19:13:26 -0500

    Ejercicio de ordenamiento Quicksort con librerias

---
+++ librerias/ingresar.h
@@ -0,0 +1,18 @@
+
+#include  <iostream>
+using namespace std;
+
+
+namespace imprimir
+{
+
+        void imprimirArreglo(int arr[], int n)
+        {
+
+            for (int i=0; i<10; i++)
+            {
+                   cout<<"|"<<arr[i]<<"|";
+            }
+
+        }
+}
